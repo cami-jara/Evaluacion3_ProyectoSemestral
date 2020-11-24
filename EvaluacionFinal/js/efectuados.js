1014f058ecfd492cd9b90e704f7365921e1c547c
cami-jara <camilati.jarase@gmail.com>
2020-11-24 19:51:59 -0300

    Proyecto Semestral Final, dos páginas ocultas

--- EvaluacionFinal/js/efectuados.js
+++ EvaluacionFinal/js/efectuados.js
@@ -1,8 +1,16 @@
 var contactos = JSON.parse(window.localStorage.getItem("contactos"))
     var mensajes = " "
     contactos.forEach((value) => {
-        mensajes = mensajes + "<span>"+value.mensaje+"</span></br>"
+        mensajes = mensajes + "<h2>"+value.nombre+"</h2>";
+        mensajes = mensajes + "<h2>"+value.apellido+"</h2>";
+        mensajes = mensajes + "<h2>"+value.correo+"</h2>";
+        mensajes = mensajes + "<h2>"+value.numero+"</h2>";
+        mensajes = mensajes + "<h2>"+value.mensaje+"</h2>";
         console.log(mensajes)
+        console.log(mensajes)
+        console.log(mensajes);
     })
     console.log(mensajes)
     document.getElementById("messages").innerHTML = mensajes;
+    localStorage.clear();
+
--- EvaluacionFinal/js/google.js
+++ EvaluacionFinal/js/google.js
@@ -21,12 +21,3 @@ function onSignIn(googleUser) {
     console.log(localStorage);
     
   }
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      window.localStorage.clear();
-      window.location.href='login.html';
-       console.log('User signed out.');
-    });
-  }
- 
